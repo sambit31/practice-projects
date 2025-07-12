86ddd90c8b4c78d8684831deba421b12910db822
Sambit Ghosh <sambitg31@gmail.com>
2025-07-13 00:15:01 +0530

    setattribute getattribute

---
+++ JAVASCRIPTS/DOM/day-3/script.js
@@ -0,0 +1,11 @@
+
+var img1 = document.querySelector("#img1");
+var img2  = document.querySelector("#img2");
+
+swapBtn.addEventListener("click",function(){
+    var img1src = img1.getAttribute("src");
+    var img2src = img2.getAttribute("src");
+    img1.setAttribute("src",img2src);
+    img2.setAttribute("src",img1src);
+
+})
